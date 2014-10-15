1fbf86acf05dbfc8805d3aa974dfe781ddc93715
Tommaso Barbugli <tbarbugli@gmail.com>
2014-10-15 17:21:02 +0200

    add first bunch of tests

--- src/GetStream/StreamLaravel/FeedManager.php
+++ src/GetStream/StreamLaravel/FeedManager.php
@@ -18,16 +18,19 @@ class StreamLaravelManager {
         $this->userFeed = $this->config->get("stream-laravel::user_feed");
     }
 
-    public function getUserFeed($user_id){
+    public function getUserFeed($user_id)
+    {
         return $this->client->feed("$this->userFeed:$user_id");
     }
 
-    public function getNotificationFeed($user_id){
+    public function getNotificationFeed($user_id)
+    {
         $user_feed = $this->config->get("stream-laravel::notification_feed");
         return $this->client->feed("$user_feed:$user_id");
     }
 
-    public function getNewsFeeds($user_id){
+    public function getNewsFeeds($user_id)
+    {
         $feeds = array();
         $news_feeds = $this->config->get("stream-laravel::news_feeds");
         foreach ($news_feeds as $feed) {
---
+++ tests/ManagerTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace GetStream\StreamLaravel;
+use Mockery as m;
+
+class ManagerTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function setUp(){
+        $config = m::mock('ConfigMock');
+        $config->shouldReceive('get')->once()->with('stream-laravel::user_feed')
+            ->andReturn('user');
+        $config->shouldReceive('get')->once()->with('stream-laravel::notification_feed')
+            ->andReturn('notification');
+        $config->shouldReceive('get')->once()->with('stream-laravel::news_feeds')
+            ->andReturn(array('flat'=>'flat', 'aggregated'=>'aggregated'));
+        $this->manager = new StreamLaravelManager('key', 'secret', $config);
+    }
+
+    public function testGetUserFeed(){
+        $feed = $this->manager->getUserFeed(42);
+        $this->assertSame($feed->getFeedId(), 'user:42');
+    }
+
+    public function testGetNotificationFeed()
+    {
+        $feed = $this->manager->getNotificationFeed(42);
+        $this->assertSame($feed->getFeedId(), 'notification:42');
+    }
+
+    public function testGetNewsFeeds()
+    {
+        $feeds = $this->manager->getNewsFeeds(42);
+        $this->assertSame($feeds['aggregated']->getFeedId(), 'aggregated:42');
+        $this->assertSame($feeds['flat']->getFeedId(), 'flat:42');
+    }
+
+    public function testFollowUser()
+    {
+    }
+
+    public function testUnfollowUser()
+    {
+    }
+
+    public function testGetFeed()
+    {
+        $feed = $this->manager->getFeed('myfeed', 42);
+        $this->assertSame($feed->getFeedId(), 'myfeed:42');
+    }
+
+    public function testActivityCreated()
+    {
+    }
+
+    public function testActivityDeleted()
+    {
+    }
+
+}
