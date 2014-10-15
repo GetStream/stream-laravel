9124b009ee66a4600233d6388e8d2f5cd1ba17e4
Tommaso Barbugli <tbarbugli@gmail.com>
2014-10-15 20:42:42 +0200

    add doc blocks

--- src/GetStream/StreamLaravel/Eloquent/Activity.php
+++ src/GetStream/StreamLaravel/Eloquent/Activity.php
@@ -6,63 +6,115 @@ abstract class Activity extends Model {
 
     protected static $activitySyncPolicy = '\GetStream\StreamLaravel\Eloquent\CreateRemoveObserver';
 
+    /**
+     * The name of the feed type where models activities should be stored
+     * leave it empty to default to global user feed
+     * @return string
+     */
     public function activityAuthorFeed()
     {
         return null;
     }
 
+    /**
+     * Returns an array of relations as strings or functions that tell the enrich
+     * class which (and how) related models should be loaded. By default the result
+     * of this is passed directly to Eloquent's query With lazy loader method
+     * @return array
+     */
     public function activityLazyLoading()
     {
         return array();
     }
 
+    /**
+     * The extra data that should be part of the activity. The extra data will be serialized
+     * using PHP's built-in json_encode function
+     * @return array[string]
+     */
     public function activityExtraData()
     {
         return null;
     }
 
+    /**
+     * The name of the property that holds a reference to the author/owner of the model instance
+     * @return string
+     */
     public function activityActorMethodName()
     {
         return 'user';
     }
 
+    /**
+     * The id of the author/owner of the model instance
+     * @return int|string
+     */
     public function activityActorId()
     {
         $actor = $this->{$this->activityActorMethodName()};
         return $actor->id;
     }
 
+    /**
+     * The reference to the model instance of the author/owner
+     * @return object \Illuminate\Database\Eloquent\Model instance
+     */
     public function activityActor()
     {
         $actor = $this->{$this->activityActorMethodName()};
         return Utils::createModelReference($actor);
     }
 
+    /**
+     * The activity verb for this instance
+     * @return string
+     */
     public function activityVerb()
     {
         return strtolower(get_called_class());
     }
 
+    /**
+     * The activity object for this instance
+     * @return string
+     */
     public function activityObject()
     {
         return Utils::createModelReference($this);
     }
 
+    /**
+     * The activity foreign_id for this instance
+     * @return string
+     */
     public function activityForeignId()
     {
         return $this->activityObject();
     }
 
+    /**
+     * The activity time for this instance
+     * @return string
+     */
     public function activityTime()
     {
         return $this->created_at->format(\DateTime::ISO8601);
     }
 
+    /**
+     * The feeds that should receive a copy of this instance when it's created
+     * @return GetStream\Stream\Feed[]
+     */
     public function activityNotify()
     {
         return null;
     }
 
+    /**
+     * The activity data for this instance
+     * @return array[string]string
+     */
     public function createActivity()
     {
         $activity = array();
@@ -74,7 +126,10 @@ abstract class Activity extends Model {
 
         $to = $this->activityNotify();
         if ($to !== null){
-            $activity['to'] = $to;
+            $activity['to'] = array();
+            foreach ($to as $feed) {
+                $activity['to'][] = $to->getFeedId();
+            }
         }
 
         $extra_data = $this->activityExtraData();
