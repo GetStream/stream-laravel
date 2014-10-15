046fe22bf21ec1f09af218ceffbca4c3138ed266
Tommaso Barbugli <tbarbugli@gmail.com>
2014-10-15 17:33:18 +0200

    fix silly view mistake

--- src/views/render_activity.blade.php
+++ src/views/render_activity.blade.php
@@ -1,4 +1,4 @@
-@if ($prefix)
+@if (isset($prefix))
     @if (array_key_exists('activities', $activity))
         @include("aggregated_activity.{$prefix}_{$activity['verb']}", array('activity'=>$activity))
     @else
